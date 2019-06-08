89a87d46ec1016307e53a3c0535a0021c6e3313f
Sophie OT <SophieOT@D-10-19-199-253.dhcp4.washington.edu>
2019-06-08 12:06:31 -0700

    added schema with 2 tables to start with, added a seeds file

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,20 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+create database taskdb;
+use taskdb;
+
+create table user (
+    id AUTO_INCREMENT NOT NULL,
+    user_name VARCHAR(50),
+    primary key(id)
+);
+
+drop table tasks if exists;
+create table tasks (
+    id int not null,
+    task_name varchar(250),
+    complete boolean,
+    foreign key (id) references user(id)
+)
