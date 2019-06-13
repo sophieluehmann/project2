57cb363f62814aa642c7aea52151c9f697ad821a
Wendy Wigg <wiggwendy@gmail.com>
2019-06-13 10:41:12 -0700

    added incomplete button to move tasks to incomplete list

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -44,7 +44,6 @@ module.exports = function(app) {
     })
       .then(function(dbTasks) {
         console.log(dbTasks);
-        //res.status(200).send(`Updated task: ${dbTasks}`);
         res.redirect("/");
       })
       .catch(function(err) {
@@ -67,6 +66,26 @@ module.exports = function(app) {
       .then(function(dbTasks) {
         console.log(dbTasks);
         res.status(200).send(`Completed task: ${dbTasks}`);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send(`Server Error: ${err}`);
+      });
+  });
+
+  // Change completed task to incomplete
+  app.put("/api/tasks/incomplete/:id", function(req, res) {
+    db.Task.update(
+      { complete: false },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(function(dbTasks) {
+        console.log(dbTasks);
+        res.status(200).send(`Task moved to incomplete: ${dbTasks}`);
       })
       .catch(function(err) {
         console.error(err);
